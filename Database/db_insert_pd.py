742ef13ecba0eb81764ec1f68b89082f6e17615c
shulya403 <56358220+shulya403@users.noreply.github.com>
2020-08-02 19:27:23 +0300

    db works Nb_products insert, update

--- Database/db_insert_pd.py
+++ Database/db_insert_pd.py
@@ -2,6 +2,27 @@ import pandas as pd
 import sqlalchemy as sql
 import json
 
+# Изменяемые записи для Update
+def df_compare(df_old, df_new):
+
+    if set(df_old.columns) == set(df_new.columns):
+        exit_ = pd.DataFrame(columns=df_old.columns)
+        exit_loc = 0
+        cols = set(df_old.columns) - {'name'}
+
+        for i, row in df_new.iterrows():
+            for j in cols:
+                name = row['name']
+                if df_old[df_old['name'] == name][j].values[0] != row[j]:
+                    exit_.loc[exit_loc] = row
+                    exit_loc += 1
+    else:
+        print("Error df_compare(df_old, df_new): Поля несовпадают")
+        raise
+
+    return exit_
+
+
 class DB_insert_from_excel(object):
     def __init__ (self,
                     xl_Products,
@@ -114,22 +135,92 @@ class DB_insert_from_excel(object):
         except KeyError:
             xl_page = ""
 
-        #xl_df_Products = Get_xl(xl_page, dir_root + Category + "/" + xl_Products)
+        xl_df_Products = Get_xl(xl_page, dir_root + Category + "/" + xl_Products)
 
-        #self.df_Products = df_Products(xl_df_Products, self.dict_xl_db_Fields)
-        #self.df_Classes = df_Classes(xl_df_Products, self.dict_xl_db_Classes)
+        self.df_Products = df_Products(xl_df_Products, self.dict_xl_db_Fields)
+        self.df_Classes = df_Classes(xl_df_Products, self.dict_xl_db_Classes)
 
 # Databse connect
-        self.sql_engine = sql.create_engine("mysql://shulya403:1qazxsw2@localhost/all_gid_2", echo=True)
+    def DB_alchemy(self,
+                   category,
+                   db="mysql://shulya403:1qazxsw2@localhost/all_gid_2"):
+        self.sql_engine = sql.create_engine(db, echo=True)
 
         print(self.sql_engine.table_names())
+        metadata = sql.MetaData(self.sql_engine)
+        sql_tbl_name_products = category+'_products'
+        self.tbl_products = sql.Table(sql_tbl_name_products, metadata, autoload=True)
 
-# Заливка Pruducts
-    def Insert_SQL_Entity(self, df):
-        pass
+        self.connection = self.sql_engine.connect()
 
 
-# Скачивание из Datatbase
+# Заливка df в tbl SQL
+    def Insert_df_to_SQL(self, df, tbl):
+
+            dict_insert = df.to_dict(orient='records')
+
+            insert_qry = tbl.insert()
+            self.connection.execute(insert_qry, dict_insert)
+
+    def Update_df_in_SQL(self,df, tbl):
+
+        cols = set(df.columns) - {'name'}
+        cols = list(cols)
+        for name in df['name']:
+            dict_upadate = df[df['name'] == name][cols].to_dict(orient='records')
+            update_qry = tbl.update().where(tbl.c.name == name).values()
+            self.connection.execute(update_qry, dict_upadate)
+
+
+# df из из таблицы SQL
+    def Select_SQL_to_df(self, tbl):
+
+        exit_ = pd.DataFrame()
+        select_qry = tbl.select()
+        exit_ = pd.read_sql(select_qry, self.connection)
+
+        return exit_
+
+# df имеющиеся продукты
+    def Select_SQL_Products(self):
+        df_ = self.Select_SQL_to_df(self.tbl_products)
+        #print(df_.head(5))
+
+        return df_
+
+# сопоставление двух df. новые по полю name
+    def New_names(self, df_old, df_new):
+
+        #Новые записи
+        df_old_names = set(df_old['name'])
+        df_new_names = set(df_new['name'])
+
+        difference = df_new_names - df_old_names
+        coincidence = df_old_names - difference
+
+        exit_insert = df_new[df_new['name'].isin(difference)]
+
+        #Проверка исправленнных
+
+        df_old_names_restrict = df_old[df_new.columns]
+
+        #exit_update = df_new.compare(df_old_names_restrict)
+        exit_update = df_compare(df_old_names_restrict, df_new[df_new['name'].isin(coincidence)])
+
+        return (exit_insert, exit_update)
+
+# Products_handle
+    def Pruducts_to_SQL(self, df_new):
+
+        tup_df = self.New_names(self.Select_SQL_Products(), df_new)
+        df_select = tup_df[0]
+        df_update = tup_df[1]
+
+        if not df_select.empty:
+            self.Insert_df_to_SQL(df_select, self.tbl_products)
+
+        if not df_update.empty:
+            self.Update_df_in_SQL(df_update, self.tbl_products)
 
 
 
@@ -141,7 +232,10 @@ class DB_insert_from_excel(object):
 #                     dir_root = "C:\\Users\\shulya403\\Shulya403_works\\all_gid_2\\Data\\",
 #                     Category='Nb',
 #                     JSON_file="categories_fields.json"):
-
-DB_insert_from_excel(xl_Products="nb_models_06_new.xlsx",
+print(pd.__version__)
+FillDB = DB_insert_from_excel(xl_Products="nb_models_06_new.xlsx",
                      xl_Vardata="NB_Report-5`20.xlsx")
+FillDB.DB_alchemy(FillDB.Category)
+#FillDB.Insert_SQL_Entity(FillDB.df_Products)
+FillDB.Pruducts_to_SQL(df_new=FillDB.df_Products.head(25))
 
