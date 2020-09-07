039cabb757464ae5cf2f148de4fc51e269d61400
shulya403 <56358220+shulya403@users.noreply.github.com>
2020-09-07 17:07:00 +0300

    07.09 django и заливка бд Сделан dump данных Исправлена ошибка с таблицами MntProducts Добавлен Autochange для содержания полей xl JSON Mnt, Mfp Залиты три категории
    Кривая форма генируремая во views.ru как html
    Работает фильтр

--- Database/dump/join_distinct.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM all_gid_2.nb_products AS pd
-INNER JOIN (SELECT DISTINCT fk_products FROM 
-(SELECT * FROM all_gid_2.nb_products_has_nb_classes AS mtm1
-WHERE fk_classes in (1, 3, 10)) AS mtm2) 
-AS mtm
-ON pd.id = mtm.fk_products;
