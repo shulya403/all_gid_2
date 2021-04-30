aaeb8bacc490c7fc14d27231155ee654b7bab0bc
shulya403 <56358220+shulya403@users.noreply.github.com>
2021-04-30 20:33:34 +0300

    30.04 с началом мобильзации продуктов

--- all_gid_2/marketability/views.py
+++ all_gid_2/marketability/views.py
@@ -48,7 +48,7 @@ def digit_separator(digit):
         exit_ = str()
         tail = len(str_digit) % 3
         for i in range(len(str_digit)-3, -1, -3):
-            exit_ = " " + str_digit[i:i+3] + exit_
+            exit_ = "\xa0" + str_digit[i:i+3] + exit_
 
         if tail != 0:
             exit_ = str_digit[:tail] + exit_
