20a3918bf86471123320c6d0eda0729c97404bb7
shulya403 <56358220+shulya403@users.noreply.github.com>
2021-09-19 19:21:42 +0300

    19/09 с началом страницы вендоров. Глючит на пробельных брендах. Таблица не выведена

--- all_gid_2/marketability/ven_views.py
+++ all_gid_2/marketability/ven_views.py
@@ -11,34 +11,49 @@ def vendor(request, cat_, vendor_=""):
 
     try:
         categories_list = request.session['categories_list']
+        category_name = request.session['cat_rus_name']
 
     except KeyError:
         ctg = views.Init_cat(request, cat_, db_tbl)
         categories_list = request.session['categories_list']
+        category_name = request.session['cat_rus_name']
     str_period_inbase = request.session['period_inbase']
     period_inbase = views.Recover_Date_period_inbase(str_period_inbase)
 
+    vardata_kwargs = {cat_.lower() + 'vardata__month__in': period_inbase}
+    vendors_list = db_tbl['products'].objects.filter(**vardata_kwargs).values_list(
+        'brand').distinct().order_by('brand')
+    print(vendors_list)
+
     if not vendor_:
-        vendor_ = db_tbl['products'].objects.values_list('brand').distinct()[0][0]
+        vendor_ = views.vlist_to_list(vendors_list)[0]
+        print(vendor_)
 
 
     #qry = db_tbl['products'].objects.filter(id=2653)[0].classes_mtm.values('name')
-    GeneralQry = db_tbl['products'].objects.filter(brand=vendor_, nbvardata__month__in=period_inbase).values()
+    brand_vardata_kwargs = {'brand': vendor_, cat_.lower() + 'vardata__month__in': period_inbase}
+    GeneralQry = db_tbl['products'].objects.filter(**brand_vardata_kwargs).values()
     print(len(GeneralQry))
-    qry = db_tbl['products'].objects.prefetch_related('classes_mtm').values('name', 'classes_mtm__text')
-    print(qry[0:10])
+    #qry = db_tbl['products'].objects.prefetch_related('classes_mtm').values('name', 'classes_mtm__text')
+    #print(qry[0:10])
 
     #start_time = time.monotonic()
-    list_classes_GO = list()
+    list_classes_GO_CL = list()
     for subtype in request.session['new_form']['GO']:
         for clg in [cl['text'] for cl in request.session['new_form']['GO'][subtype]]:
-                list_classes_GO.append(clg)
-    print(list_classes_GO)
+                list_classes_GO_CL.append(clg)
+    for subtype in request.session['new_form']['CL']:
+        for clg in [cl['text'] for cl in request.session['new_form']['CL'][subtype]]:
+            list_classes_GO_CL.append(clg)
+
+    print(list_classes_GO_CL)
 
     start_time = time.monotonic()
-    for goal in list_classes_GO:
+    #classes_mtm_list = views.vlist_to_list(qry.filter(name=GeneralQry[0]['name']).values_list('classes_mtm__text'))
+    #print(classes_mtm_list)
+    for goal in list_classes_GO_CL:
         for i in range(len(GeneralQry)):
-            GeneralQry[i][goal] = (goal in views.vlist_to_list(qry.filter(name=GeneralQry[i]['name']).values_list('classes_mtm__text')))
+            GeneralQry[i][goal] = (goal in list_classes_GO_CL)
     print(time.monotonic()-start_time)
     print(GeneralQry[0])
 
@@ -51,5 +66,20 @@ def vendor(request, cat_, vendor_=""):
     #             print(qry.filter(name=name_, classes_mtm__text=goal))
     # print(time.monotonic()-start_time)
     # print(GeneralQry[0])
+    fieleds_ttx = list()
+    for fld in GeneralQry[0].keys():
+        if fld not in (set(list_classes_GO_CL) | {'id', 'brand'}):
+            fieleds_ttx.append(fld)
 
-    return render(request, template_name="ttx_how.html", context={})
+    exit_ = {
+        'categories_list': categories_list,
+        'category_name': category_name,
+        'action': cat_,
+        'vendors_list': views.vlist_to_list(vendors_list),
+        'vendor': vendor_,
+        'table': GeneralQry,
+        'fieleds_ttx': fieleds_ttx,
+        'fields_classes': list_classes_GO_CL
+    }
+
+    return render(request, template_name="vendors.html", context=exit_)
